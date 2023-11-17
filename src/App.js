4a7099cd52ea84780571d66df2b48b87a3d1c107
Hyewon Lee <vndnsla12@gmail.com>
2023-11-18 03:58:11 +0900

    추천서버 확인

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  
+
+  const [num, setNum] = React.useState('-')
+  const [state, setState] = React.useState(false)
+
+  const onClick = ()=> {
+    // 1~40(1-39채 및 20세 이상 채널) / 0 제외
+    let random = Math.floor(Math.random() * (40 - 1 + 1)) + 1
+    let channel = random === 2 ? '20세 이상 ' : random > 2 ? `${random-1}` : `${random}`
+    setNum(channel)
+
+    // true: ~ 채널 문구, 다시 고르기 show / 버튼 hide
+    // false: ~ 채널 문구, 다시 고르기 hide / 버튼 show
+    setState(true)
+
+  }
+
+  const undoSelect = ()=> {
+    setState(false)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrap'>
+      <div className='inner'>
+        <div className='content'>
+          <strong className='logo-name'>Maplestory</strong>
+          <div className='recommend-server-box'>
+            <span>이번 주 추천 서버는?</span>
+            
+            {/* 채널 선택 버튼 */}
+            <button type='button' onClick={onClick} className={`btn ${state ? 'd-none' : ''}`}>추천 서버 확인하기 <i className="ri-arrow-right-s-line"></i></button>
+
+            {/* 채널 결과 */}
+            <strong className={`server-result ${state ? '' : 'd-none'}`}>{`${state ? num + '채널' : '-채널'}`}</strong>
+          </div>
+
+          {/* 다시 고르기 */}
+          <div className={`undo-box ${state ? '' : 'd-none'}`}>
+            <button type='button' onClick={undoSelect} className='btn-undo'>다시 고르기 <i className="ri-arrow-right-s-line"></i></button>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
